efbd6755d7e06a96534b409a3fa13f948c2a9085
ovfisher <ovfisher@gmail.com>
2024-10-17 17:30:55 +0300

    inter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+#tasklist = ["t1","t2","t3","t4"]
 class Task:
     def __init__(self, name, discr, days, status):
         self.name = name
@@ -11,10 +12,24 @@ class Task:
         self.name = 'name'
         self.discr = 'discr'
         self.days = 30
-        self.status = 'in progress'
+        self.status = 'active'
 
     def closetask(self):
         self.status = 'done'
+        self.days = 0
 
     def listtask(self):
-        print('list task in progress')
+        if self.status == 'active':
+            print(f'list task in progress {self.name}')
+  #      #for i in range(len(list)):
+#            print(f'task in progress - {i + 1}: {list[i]}')
+
+t1 = Task('t1', 'task1', '20', 'active' )
+t2 = Task('t2', 'task2', '30', 'active' )
+t3 = Task('t3', 'task3', '30', 'active' )
+
+t2.closetask()
+
+instances = [t1, t2, t3]
+for instance in instances:
+    print(instance.listtask())
