69b9c8f59fb98815c3ad76e34c30218a1ad1f610
ovfisher <ovfisher@gmail.com>
2024-10-17 19:30:37 +0300

    final

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 
 #
 class Task:
-    def __init__(self, name, discr, days, status):
+    def __init__(self, name, descr, days, status):
         self.name = name
-        self.discr = discr
+        self.descr = descr
         self.days = days
         self.status = status
-
-    def atask(task):
-        tname = input('enter task name - ')
-        tdiscr = input('enter task discription - ')
-        tdays = input('enter task term (in days) - ')
-
-        task.name = tname
-        task.discr = tdiscr
-        task.days = tdays
-        task.status = 'active'
 
     def closetask(self):
         self.status = 'done'
@@ -24,9 +14,8 @@ class Task:
     def listtask(self):
 #       if self.status == 'active':
             print(f'list task in progress {self.name}')
-#      #for i in range(len(list)):
-#       print(f'task in progress - {i + 1}: {list[i]}')
 
+# init lists of class Task objects
 instances = []
 tx = Task('t1', 'task1', '20', 'active' )
 instances.append(tx)
@@ -34,15 +23,18 @@ tx = Task('t2', 'task2', '30', 'active' )
 instances.append(tx)
 tx = Task('t3', 'task3', '30', 'active' )
 instances.append(tx)
+
+# update status for first task as have done
 instances[0].closetask()
 
 def addtask ():
     tname = input('enter task name - ')
-    tdiscr = input('enter task discription - ')
+    tdescr = input('enter task description - ')
     tdays = input('enter task term (in days) - ')
-    result = Task(tname, tdiscr, tdays, 'active')
+    result = Task(tname, tdescr, tdays, 'active')
     return result
 
+# add new task by console
 tx = addtask()
 instances.append(tx)
 
