360438f7db901578361c620312978ba23d41c903
ovfisher <ovfisher@gmail.com>
2024-10-17 19:20:14 +0300

    task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-#tasklist = ["t1","t2","t3","t4"]
+#
 class Task:
     def __init__(self, name, discr, days, status):
         self.name = name
@@ -7,29 +7,45 @@ class Task:
         self.days = days
         self.status = status
 
-    def opentask(self):
-        print('new task')
-        self.name = 'name'
-        self.discr = 'discr'
-        self.days = 30
-        self.status = 'active'
+    def atask(task):
+        tname = input('enter task name - ')
+        tdiscr = input('enter task discription - ')
+        tdays = input('enter task term (in days) - ')
+
+        task.name = tname
+        task.discr = tdiscr
+        task.days = tdays
+        task.status = 'active'
 
     def closetask(self):
         self.status = 'done'
         self.days = 0
 
     def listtask(self):
-        if self.status == 'active':
+#       if self.status == 'active':
             print(f'list task in progress {self.name}')
-  #      #for i in range(len(list)):
-#            print(f'task in progress - {i + 1}: {list[i]}')
+#      #for i in range(len(list)):
+#       print(f'task in progress - {i + 1}: {list[i]}')
 
-t1 = Task('t1', 'task1', '20', 'active' )
-t2 = Task('t2', 'task2', '30', 'active' )
-t3 = Task('t3', 'task3', '30', 'active' )
+instances = []
+tx = Task('t1', 'task1', '20', 'active' )
+instances.append(tx)
+tx = Task('t2', 'task2', '30', 'active' )
+instances.append(tx)
+tx = Task('t3', 'task3', '30', 'active' )
+instances.append(tx)
+instances[0].closetask()
 
-t2.closetask()
+def addtask ():
+    tname = input('enter task name - ')
+    tdiscr = input('enter task discription - ')
+    tdays = input('enter task term (in days) - ')
+    result = Task(tname, tdiscr, tdays, 'active')
+    return result
 
-instances = [t1, t2, t3]
+tx = addtask()
+instances.append(tx)
+
 for instance in instances:
-    print(instance.listtask())
+    if instance.status == 'active':
+        print(instance.listtask())
